0a73b30e8d74aae2b328c09bdc51ced1d150a4f2
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-20 11:24:41 +0530

    refactor: standardize string quotes in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 // backend/config/db.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
